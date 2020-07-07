23c8baf2a3cf25f8e3afad7c2c84cac1514fd99d
Neto Martins <netomartins2@hotmail.com>
2020-07-07 20:54:20 -0300

    iniciando o projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+ 
+        <p>
+          Iniciando o projeto OLX
+
+          <br/>
+          <br/>
+
+          iM3 Web
+
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
